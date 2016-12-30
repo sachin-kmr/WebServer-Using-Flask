e50e378a499ec5fef604f02797c5f4ca2cc7bc86
Sachin Kumar <sachink.it@nsit.net.in>
2016-12-30 17:06:18 +0530

    Restaurant Menus Added

--- project.py
+++ project.py
@@ -1,10 +1,29 @@
 from flask import Flask 
 app = Flask(__name__)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	return "Hello World!"
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+	#restaurant = session.query(Restaurant).all()[0]
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id)[0]
+	items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+	output = ''
+	for i in items:
+		output += i.name
+		output += '</br>'
+		output += i.price
+		output += '</br>'
+		output += i.description
+		output += '</br><br>'
+	return output
 
 if __name__ == '__main__':
 	app.debug = True
