6e3ed4e90f6786c3264ff599116391c7037508c6
Sachin Kumar <sachink.it@nsit.net.in>
2016-12-30 18:13:58 +0530

    added render_template

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 app = Flask(__name__)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -13,17 +13,30 @@ session = DBSession()
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
 	#restaurant = session.query(Restaurant).all()[0]
-	restaurant = session.query(Restaurant).filter_by(id=restaurant_id)[0]
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-	output = ''
-	for i in items:
-		output += i.name
-		output += '</br>'
-		output += i.price
-		output += '</br>'
-		output += i.description
-		output += '</br><br>'
-	return output
+	# output = '<h1><center> %s </center></h1>' %restaurant.name
+	# for i in items:
+	# 	output += i.name
+	# 	output += '</br>'
+	# 	output += i.price
+	# 	output += '</br>'
+	# 	output += i.description
+	# 	output += '</br><br>'
+	# return output
+	return render_template('menu.html', restaurant=restaurant, items=items)
+
+@app.route('/restaurants/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+	pass
+	
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id):
+	pass
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id):
+	pass
 
 if __name__ == '__main__':
 	app.debug = True
